acf160f789074bad8841d4b34cf33c253f265d8a
Quan <nt.quan308@gmail.com>
2022-10-19 01:08:28 +0700

    Update create to do list and show on display

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,29 @@
+const database = [
+    {
+        id: 1,
+        title: 'Nguyễn Tiến Quân',
+        content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis iure tempore officia! Reiciendis officiis, maiores quae, recusandae exercitationem excepturi, non quas cum fuga facilis cupiditate totam alias! Officiis, saepe beatae.',
+    }
+];
+
+// Tạo vào hiển thị công việc
 let getBtnAdd = document.querySelector('.input-todo button');
-getBtnAdd.addEventListener("click", (e) => {
+getBtnAdd.addEventListener("click", () => {
     let getTitleInput = document.getElementById('valueInput').value;
     let getContentInput = document.getElementById('valueContentInput').value;
-    let getTitleContent = document.querySelector('.todo-content_title');
-    let content = document.querySelector('.todo-content_description');
-    let addContent = document.querySelector('.content-todo .content-row');
     let addContentTodo = document.querySelector('.content-todo');
-    addContentTodo.style.visibility = 'visible';
+    addContentTodo.style.display = 'block';
     let box_content = `
-        <b class="todo-content_title">${getTitleContent.innerHTML += getTitleInput}</b>
-        <div class="todo-content_description">${content.innerHTML = getContentInput}</div>
-        <div class="todo-button_group">
-            <button class="todo_add">Thêm</button>
-            <button class="todo_remove">Xóa</button>
-            <button class="todo_edit">Chỉnh sửa</button>
+    <div class="content-row" style="width: 300px;">
+        <p  class="todo-content_title" style="width: 300px; text-transform: uppercase; font-weight: bold; font-size: 20px; overflow-wrap: break-word; color: #fff;">${getTitleInput}</p>
+        <div class="todo-content_description" style="width: 300px; margin-bottom: 20px; overflow-wrap: break-word; color: #fff;">${getContentInput}</div>
+        <div class="todo-button_group" style="width: 300px; display: flex; justify-content: space-between;">
+            <button style="width: 30%; height: 30px; border: none; background-color: rgb(255, 255, 255); color: #000; border-radius: 5px;" class="todo_add">Thêm</button>
+            <button style="width: 30%; height: 30px; border: none; background-color: rgb(255, 255, 255); color: #000; border-radius: 5px;" class="todo_remove">Xóa</button>
+            <button style="width: 30%; height: 30px; border: none; background-color: rgb(255, 255, 255); color: #000; border-radius: 5px;" class="todo_edit">Chỉnh sửa</button>
         </div>
+    </div>
     `
-    addContent.innerHTML = box_content;
+    addContentTodo.innerHTML = box_content;
+
 })
