aa43eba7d1363f60b55e665f37949c38f772ec04
Quan <nt.quan308@gmail.com>
2022-10-20 02:13:50 +0700

    Update new to do list

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,35 @@
-const database = [
-    {
-        id: 1,
-        title: 'Nguyễn Tiến Quân',
-        content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis iure tempore officia! Reiciendis officiis, maiores quae, recusandae exercitationem excepturi, non quas cum fuga facilis cupiditate totam alias! Officiis, saepe beatae.',
+let btnAdd = document.querySelector('.input-todo button');
+let data = [];
+let countId = 0;
+btnAdd.addEventListener('click', createTask);
+function createTask(){
+    var titleValue = document.getElementById('titleInput').value;
+    var contentValue = document.getElementById('contentValue').value;
+    var viewRender = document.querySelector('.content-todo');
+    let codeId = countId + 1;
+    countId++;
+    var getData = {
+        id: codeId,
+        title: titleValue,
+        content: contentValue
     }
-];
-
-// Tạo vào hiển thị công việc
-let getBtnAdd = document.querySelector('.input-todo button');
-getBtnAdd.addEventListener("click", () => {
-    let getTitleInput = document.getElementById('valueInput').value;
-    let getContentInput = document.getElementById('valueContentInput').value;
-    let addContentTodo = document.querySelector('.content-todo');
-    addContentTodo.style.display = 'block';
-    let box_content = `
-    <div class="content-row" style="width: 300px;">
-        <p  class="todo-content_title" style="width: 300px; text-transform: uppercase; font-weight: bold; font-size: 20px; overflow-wrap: break-word; color: #fff;">${getTitleInput}</p>
-        <div class="todo-content_description" style="width: 300px; margin-bottom: 20px; overflow-wrap: break-word; color: #fff;">${getContentInput}</div>
-        <div class="todo-button_group" style="width: 300px; display: flex; justify-content: space-between;">
-            <button style="width: 30%; height: 30px; border: none; background-color: rgb(255, 255, 255); color: #000; border-radius: 5px;" class="todo_add">Thêm</button>
-            <button style="width: 30%; height: 30px; border: none; background-color: rgb(255, 255, 255); color: #000; border-radius: 5px;" class="todo_remove">Xóa</button>
-            <button style="width: 30%; height: 30px; border: none; background-color: rgb(255, 255, 255); color: #000; border-radius: 5px;" class="todo_edit">Chỉnh sửa</button>
-        </div>
-    </div>
-    `
-    addContentTodo.innerHTML = box_content;
-
-})
+    console.log(codeId);
+    data.push(getData);
+    for(let i=0;i<data.length;i++){
+        var contentRender = `
+            <div class="content-row">
+                <p  class="todo-content_title">${data[i].title}</p>
+                <div class="todo-content_description">${data[i].content}</div>
+                <div class="todo-button_group">
+                    <button>Thêm</button>
+                    <button>Xóa</button>
+                    <button>Chỉnh sửa</button>
+                </div>
+            </div>
+        `
+    }
+    viewRender.innerHTML += contentRender;
+    document.getElementById('titleInput').value = "";
+    document.getElementById('contentValue').value = "";
+    console.log(data);
+}
