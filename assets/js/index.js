c268ee92906875a9ec8e915858ed761f937c3df1
Quan <nt.quan308@gmail.com>
2022-10-22 00:51:10 +0700

    Remove projects manwork and update projects todolist

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,36 @@
-let btnAdd = document.querySelector('.input-todo button');
+let btn_add = document.querySelector('.input-todo button');
 let data = [];
-let countId = 0;
-btnAdd.addEventListener('click', createTask);
-function createTask(){
-    var titleValue = document.getElementById('titleInput').value;
-    var contentValue = document.getElementById('contentValue').value;
-    var viewRender = document.querySelector('.content-todo');
-    let codeId = countId + 1;
-    countId++;
-    var getData = {
-        id: codeId,
-        title: titleValue,
-        content: contentValue
-    }
-    console.log(codeId);
-    data.push(getData);
-    for(let i=0;i<data.length;i++){
-        var contentRender = `
-            <div class="content-row">
-                <p  class="todo-content_title">${data[i].title}</p>
-                <div class="todo-content_description">${data[i].content}</div>
-                <div class="todo-button_group">
-                    <button>Thêm</button>
-                    <button>Xóa</button>
-                    <button>Chỉnh sửa</button>
+let countID = 0;
+
+btn_add.addEventListener('click', (e) => {
+    console.log(e);
+    var getInput = document.querySelector('.input-todo input');
+    var getMessage = document.querySelector('.message')
+    let inputValue = document.getElementById('inputValue').value;
+    let viewDisplayContent = document.querySelector('.content_todo');
+    if(!inputValue){
+        getInput.style.border = '2px solid red';
+        getMessage.innerHTML = "* Bạn chưa nhập nội dung";
+    }else if(inputValue){
+        getMessage.innerHTML = ""
+        getInput.style.border = 'none';
+        var dataPush = {
+            id: countID = countID + 1,
+            content: inputValue
+        }
+        data.push(dataPush);
+        for(let i=0;i<data.length;i++){
+            var loadData = `
+                <div class="view-content">
+                <p>${data[i].content}</p>
+                <i class="bi bi-three-dots more-option"></i>
                 </div>
-            </div>
-        `
+            `
+        }
+        viewDisplayContent.innerHTML += loadData;
+        document.getElementById('inputValue').value = "";
     }
-    viewRender.innerHTML += contentRender;
-    document.getElementById('titleInput').value = "";
-    document.getElementById('contentValue').value = "";
-    console.log(data);
-}
+})
+btn_add.addEventListener('keydown', (e) => {
+    console.log(e.code)
+});
